0dfef510e95199eae85ebc7057f2ec0e63bbb69c
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-02 14:28:27 -0500

    not how I wanted to do it but got to try matching

--- 12-02/part1.py
+++ 12-02/part1.py
@@ -1,16 +1,3 @@
-# output = [(x * y) for x, y in result]
-
-# var horizontal, vertical
-# forward operation = add
-# down operation = add
-# up operation = subtract
-
-# ylist = [tuple(map(float, i.split(','))) for i in f]
-# new_input = []
-#     [new_input.append(int(i)) for i in s]
-#     return new_input
-import operator
-
 # def tuplify_input(f):
 #     # return list(map(lambda l: tuple(l.split()), f))
 #     new_list = []
@@ -23,21 +10,24 @@ def tuplify(i):
     x, y = i.split()
     return tuple([x, int(y)])
 
-def traverse(op):
-    pass
-
 def main():
     with open('input.txt', 'r') as my_file:
-        #steps = tuplify_input(my_file.readlines())
+        # steps = tuplify_input(my_file.readlines())
         steps = map(tuplify, my_file.readlines())
+    
     depth, distance = 0, 0
-    ops = {
-        'forward': operator.add,
-        'down': operator.add,
-        'up': operator.sub
-    }
-    [print(step) for step in steps]
-    print(steps)
+    for j, k in steps:
+        # do something
+        match j:
+            case 'forward':
+                distance += k
+            case 'down':
+                depth += k
+            case 'up':
+                depth -= k
+    
+    # output = [(x * y) for x, y in result]
+    print(depth * distance)
 
 if __name__ == "__main__":
     main()
