9ce09dacf5e9a76125b9c201c157ac31e575e120
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-02 22:06:37 -0500

    solved part 2

--- 12-02/part2.py
+++ 12-02/part2.py
@@ -1,29 +1,21 @@
-# def tuplify_input(f):
-#     # return list(map(lambda l: tuple(l.split()), f))
-#     new_list = []
-#     for i in f:
-#         x, y = i.split()
-#         new_list.append(tuple([x, int(y)]))
-#     return new_list
-
 def tuplify(i):
     x, y = i.split()
     return tuple([x, int(y)])
 
 def main():
     with open('input.txt', 'r') as my_file:
-        # steps = tuplify_input(my_file.readlines())
         steps = map(tuplify, my_file.readlines())
     
-    depth, distance = 0, 0
+    depth, distance, aim = 0, 0, 0
     for j, k in steps:
         match j:
             case 'forward':
                 distance += k
+                depth += aim * k
             case 'down':
-                depth += k
+                aim += k
             case 'up':
-                depth -= k
+                aim -= k
     
     # output = [(x * y) for x, y in result]
     print(depth * distance)
