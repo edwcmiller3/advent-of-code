193110fe423e822ea0a9b2b12c96a12046672063
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-05 12:35:12 -0500

    still struggling

--- 12-03/part2.py
+++ 12-03/part2.py
@@ -5,22 +5,50 @@ from collections import Counter
 # most_freq = max(c.values())
 # most_freq_elems = [e for e in c if c[e] == most_freq]
 # return 1 if len(most_freq_elems) > 1 else most_freq
+#
 
-def oxygen_generator_rating(list, position):
+def most_freq(row):
+    count = Counter(row)
+    most_elems = [e for e in count if count[e] == max(count.values())]
+    return 1 if len(most_elems) > 1 else most_elems[0]
+
+def min_freq(row):
+    count = Counter(row)
+    min_elems = [e for e in count if count[e] == min(count.values())]
+    return 0 if len(min_elems) > 1 else min_elems[0]
+
+def freq(row, compare):
+    count = Counter(row)
+    elems = [e for e in count if count[e] == compare(count.values())]
+    return compare(elems) if len(elems) > 1 else elems[0]
+
+# EXAMPLE
+# def list_sum(ls, index=0, result=0):
+#     if index == len(ls):
+#         return result
+#     return list_sum(ls, index + 1, result + ls[index])
+
+
+
+def oxygen_generator_rating(ls, index=0):
+    if index == len(ls):
+        return # SOMETHING
     # find most common bit (1 or 0)
     # if equally common, select 1
     # create list with only those where bit n is most common
     # recursion until only one list remains
     # return as int
-    return oxygen_generator_rating(list, position + 1)
+    return oxygen_generator_rating(ls, index + 1)
 
-def co2_scrubber_rating(list, position):
+
+def co2_scrubber_rating(ls, index=0):
     # find least common bit (1 or 0)
     # if equally common, select 0
     # create list with only those where bit n is most common
     # recursion until only one list remains
     # return as int
-    return co2_scrubber_rating(list, position + 1)
+    return co2_scrubber_rating(ls, index + 1)
+
 
 def main():
     with open('input.txt', 'r') as my_file:
@@ -29,7 +57,7 @@ def main():
     prepared_diagnostic = [list(x) for x in raw_diagnostic.split()]
     rotated_diagnostic = [[row[i] for row in prepared_diagnostic]
                           for i in range(len(prepared_diagnostic[0]))]
-    
+
     # todo
 
 
