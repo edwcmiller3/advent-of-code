2df9723ec0b6369bbb2cd798d718ce469c15b1a4
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-05 17:09:16 -0500

    managed to solve part 1 again instead :(

--- 12-03/part2.py
+++ 12-03/part2.py
@@ -1,26 +1,27 @@
 from collections import Counter
 
-# row = ['1', '0', '1', '0']
-# c = Counter(row)
-# most_freq = max(c.values())
-# most_freq_elems = [e for e in c if c[e] == most_freq]
-# return 1 if len(most_freq_elems) > 1 else most_freq
-#
+# def most_freq(row):
+#     count = Counter(row)
+#     most_elems = [e for e in count if count[e] == max(count.values())]
+#     return 1 if len(most_elems) > 1 else most_elems[0]
 
-def most_freq(row):
-    count = Counter(row)
-    most_elems = [e for e in count if count[e] == max(count.values())]
-    return 1 if len(most_elems) > 1 else most_elems[0]
+# def min_freq(row):
+#     count = Counter(row)
+#     min_elems = [e for e in count if count[e] == min(count.values())]
+#     return 0 if len(min_elems) > 1 else min_elems[0]
 
-def min_freq(row):
-    count = Counter(row)
-    min_elems = [e for e in count if count[e] == min(count.values())]
-    return 0 if len(min_elems) > 1 else min_elems[0]
+# def freq(row, compare):
+#     count = Counter(row)
+#     elems = [e for e in count if count[e] == compare(count.values())]
+#     return compare(elems) if len(elems) > 1 else elems[0]
 
-def freq(row, compare):
-    count = Counter(row)
-    elems = [e for e in count if count[e] == compare(count.values())]
-    return compare(elems) if len(elems) > 1 else elems[0]
+
+def hof_freq(compare):
+    def freq(row):
+        count = Counter(row)
+        elems = [e for e in count if count[e] == compare(count.values())]
+        return compare(elems) if len(elems) > 1 else elems[0]
+    return freq
 
 # EXAMPLE
 # def list_sum(ls, index=0, result=0):
@@ -29,10 +30,9 @@ def freq(row, compare):
 #     return list_sum(ls, index + 1, result + ls[index])
 
 
-
 def oxygen_generator_rating(ls, index=0):
     if index == len(ls):
-        return # SOMETHING
+        return  # SOMETHING
     # find most common bit (1 or 0)
     # if equally common, select 1
     # create list with only those where bit n is most common
@@ -58,7 +58,13 @@ def main():
     rotated_diagnostic = [[row[i] for row in prepared_diagnostic]
                           for i in range(len(prepared_diagnostic[0]))]
 
-    # todo
+    max_freq = hof_freq(max)
+    min_freq = hof_freq(min)
+
+    oxy = int(''.join([max_freq(r) for r in rotated_diagnostic]), 2)
+    co2 = int(''.join([min_freq(s) for s in rotated_diagnostic]), 2)
+
+    print(oxy * co2)
 
 
 if __name__ == "__main__":
