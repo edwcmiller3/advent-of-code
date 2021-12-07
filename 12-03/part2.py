f041308fa348b52546750f60bfd650f0bd4fc475
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-06 22:57:19 -0500

    solved day 3 finally (with help)

--- 12-03/part2.py
+++ 12-03/part2.py
@@ -1,69 +1,21 @@
 from collections import Counter
 
-# def most_freq(row):
-#     count = Counter(row)
-#     most_elems = [e for e in count if count[e] == max(count.values())]
-#     return 1 if len(most_elems) > 1 else most_elems[0]
 
-# def min_freq(row):
-#     count = Counter(row)
-#     min_elems = [e for e in count if count[e] == min(count.values())]
-#     return 0 if len(min_elems) > 1 else min_elems[0]
-
-# def freq(row, compare):
-#     count = Counter(row)
-#     elems = [e for e in count if count[e] == compare(count.values())]
-#     return compare(elems) if len(elems) > 1 else elems[0]
-
-
-def hof_freq(compare):
-    def freq(row):
-        count = Counter(row)
-        elems = [e for e in count if count[e] == compare(count.values())]
-        return compare(elems) if len(elems) > 1 else elems[0]
-    return freq
-
-# EXAMPLE
-# def list_sum(ls, index=0, result=0):
-#     if index == len(ls):
-#         return result
-#     return list_sum(ls, index + 1, result + ls[index])
-
-
-def oxygen_generator_rating(ls, index=0):
-    # find most common bit (1 or 0)
-    # if equally common, select 1
-    # create list with only those where bit n is most common
-    # recursion until only one list remains
-    # return as int
-    # From https://github.com/Vasile-Hij/AOC-2021/blob/main/scripts/3.py
-    new_ls = [l for l in ls if l[index] == f]
-    return new_ls[0] if len(new_ls) == 1 else oxygen_generator_rating(ls, index + 1)
-
-
-def co2_scrubber_rating(ls, index=0):
-    # find least common bit (1 or 0)
-    # if equally common, select 0
-    # create list with only those where bit n is most common
-    # recursion until only one list remains
-    # return as int
-    return co2_scrubber_rating(ls, index + 1)
+def ratings(ls, fn, index=0):
+    # Big thanks to https://github.com/Vasile-Hij/AOC-2021/blob/main/scripts/3.py
+    count = fn(Counter([l[index] for l in ls]).most_common(), key=lambda x: (x[1], x[0]))[0]
+    new_ls = [l for l in ls if l[index] == count]
+    return new_ls[0] if len(new_ls) == 1 else ratings(new_ls, fn, index + 1)
 
 
 def main():
     with open('input.txt', 'r') as my_file:
-        raw_diagnostic = my_file.read()
-
-    prepared_diagnostic = [list(x) for x in raw_diagnostic.split()]
-    rotated_diagnostic = [[row[i] for row in prepared_diagnostic]
-                          for i in range(len(prepared_diagnostic[0]))]
-
-    max_freq = hof_freq(max)
-    min_freq = hof_freq(min)
-
-    # oxy = int(''.join([max_freq(r) for r in rotated_diagnostic]), 2)
-    # co2 = int(''.join([min_freq(s) for s in rotated_diagnostic]), 2)
-
+        diagnostic = [line.strip() for line in my_file.readlines()]
+    
+    oxygen_generator_rating = int(ratings(diagnostic, max), 2)
+    co2_scrubber_rating = int(ratings(diagnostic, min), 2)
+    
+    print(oxygen_generator_rating * co2_scrubber_rating)
 
 
 if __name__ == "__main__":
---
+++ 12-04/part1.py
@@ -0,0 +1,13 @@
+# Part 1 Rules
+# Winning board = 1+ complete row / column of marked numbers
+# Score = sum(winning board unmarked numbers)
+# Choose first winning board and determine score for that board
+
+def main():
+    with open('input.txt', 'r') as my_file:
+        # raw_diagnostic = my_file.read()
+        pass
+
+
+if __name__ == "__main__":
+    main()
