d545e2a988ff6eb1678e3d2d99035a97cbca09a1
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-07 23:14:47 -0500

    first attempt at day 7 part 2 - incorrect

--- 12-07/part2.py
+++ 12-07/part2.py
@@ -8,3 +8,30 @@
 # from statistics import mean
 # or sum([calc_fuel(crab, least_cost_pos) for crab in crabs]) / len(crabs)
 # probably slap ceil() around that as well
+
+from math import ceil
+from statistics import mean
+
+
+def calc_least_cost_pos(ls):
+    return ceil(mean(ls))
+
+
+def calc_fuel(c, pos):
+    fuels = [i for i in range(1, ceil(abs(c - pos) + 1))]
+    return sum(fuels)
+
+
+def main():
+    with open('input.txt', 'r') as crab_file:
+        crabs = list(map(int, crab_file.readline().split(',')))
+
+    least_cost_pos = calc_least_cost_pos(crabs)
+
+    fuel_costs = sum([calc_fuel(crab, least_cost_pos) for crab in crabs])
+
+    print(fuel_costs)
+
+
+if __name__ == "__main__":
+    main()
