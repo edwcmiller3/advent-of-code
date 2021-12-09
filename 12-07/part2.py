2c6d3acb6d2b010d3b1ffe03214158d340ee7b2f
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-08 22:31:53 -0500

    solved day 7 part 2

--- 12-07/part2.py
+++ 12-07/part2.py
@@ -1,25 +1,15 @@
-# for each starting point
-# calc distance to lowest cost point
-# create a list of the range
-# e.g. [i for i in range(distance + 1)]
-# sum the range to calculate total fuel cost for that point
-# next starting point
-
-# from statistics import mean
-# or sum([calc_fuel(crab, least_cost_pos) for crab in crabs]) / len(crabs)
-# probably slap ceil() around that as well
-
-from math import ceil
 from statistics import mean
 
 
 def calc_least_cost_pos(ls):
-    return ceil(mean(ls))
+    return int(mean(ls))
 
 
 def calc_fuel(c, pos):
-    fuels = [i for i in range(1, ceil(abs(c - pos) + 1))]
-    return sum(fuels)
+    # Triangle numbers maths
+    # http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/runsums/triNbProof.html
+    fuels = (abs(c - pos) * (abs(c - pos) + 1)) // 2
+    return fuels
 
 
 def main():
