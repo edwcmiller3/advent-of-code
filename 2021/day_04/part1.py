53e9e76f6f77de44e8931adcd3d49614a7ce4717
edwcmiller3 <edwcmiller3@outlook.com>
2021-12-09 23:59:56 -0500

    going back and refactoring old solutions

--- 2021/day_04/part1.py
+++ 2021/day_04/part1.py
@@ -2,6 +2,7 @@
 # Winning board = 1+ complete row / column of marked numbers
 # Score = sum(winning board unmarked numbers)
 # Choose first winning board and determine score for that board
+import pprint
 
 def main():
     with open('numbers.txt', 'r') as numbers:
@@ -11,10 +12,15 @@ def main():
     #     boards = board_set.read().split('\n\n')
 
     with open('boards.txt', 'r') as board_set:
-        boards = [[c for c in l.split() if c != '\n\n'] for l in board_set.readlines()]
+        #boards = [[c for c in l.split() if c != '\n\n'] for l in board_set.readlines()]
+        boards = board_set.read().split('\n\n')
 
-    print(draw)
-    print(boards)
+    formatted_board = [[line.split() for line in board.split('\n')] for board in boards]
+    final_board = [[[int(elt) for elt in line] for line in board] for board in formatted_board]
+
+    #print(draw)
+    #print(boards)
+    pprint.pprint(final_board)
 
 
 if __name__ == "__main__":
