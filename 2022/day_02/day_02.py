09a2acbc1eee840e9852d284d32d994e68c31870
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-03 12:15:47 -0500

    day 2 part 1 solved

--- 2022/day_02/day_02.py
+++ 2022/day_02/day_02.py
@@ -1,28 +1,35 @@
 from typing import TextIO, Tuple
 
-# OPPONENT
-# A: Rock
-# B: Paper
-# C: Scissor
-#
-# YOU
-# X: Rock
-# Y: Paper
-# Z: Scissor
-# 
-# SCORING
-# Rock = 1
-# Paper = 2
-# Scissor = 3
-# Loss = 0
-# Tie = 3
-# Win = 6
+
+SHAPE_SCORE = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
 
 def parse_input(file: TextIO) -> Tuple[Tuple[str]]:
     return tuple(tuple(rounds.split(' ')) for rounds in file.read().strip().split("\n"))
 
-# def score
+
+def score_shape(shape: str) -> int:
+    return SHAPE_SCORE[shape]
+
+
+def score_outcome(round: Tuple) -> int:
+    match round:
+        case (('A', 'Y') | ('B', 'Z') | ('C', 'X')):
+            return 6
+        case (('A', 'Z') | ('B', 'X') | ('C', 'Y')):
+            return 0
+        case _:
+            return 3
+
+
+def score_round(round: Tuple) -> int:
+    return score_outcome(round) + score_shape(round[1])
+
 
 if __name__ == "__main__":
-    with open("input.txt", "r") as f:
-        parse_input(f)
+    with open("input.txt", 'r') as f:
+        print(f"Part 1: {sum(tuple(score_round(r) for r in parse_input(f)))}")
