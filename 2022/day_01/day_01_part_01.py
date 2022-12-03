3ce1fd5d5a1027ae7e86231e648f0ff4b1073dc4
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-02 23:51:28 -0500

    day 1 part 1 complete

--- 2022/day_01/day_01_part_01.py
+++ 2022/day_01/day_01_part_01.py
@@ -1,5 +1,18 @@
-from ..utils import parse_input
-from typing import *
+from typing import TextIO, Tuple
 
-parse_input("asdf")
 
+def parse_input(file: TextIO) -> Tuple[Tuple[str]]:
+    return tuple(tuple(groups.split("\n")) for groups in file.read().strip().split("\n\n"))
+
+
+def tuple_str_to_int(tupstr: Tuple[Tuple[str]]) -> Tuple[Tuple[int]]:
+    # return tuple(int(item) for item in tupstr)
+    return tuple(tuple(map(int, x)) for x in tupstr)
+
+
+def solve(input: Tuple[Tuple[int]]) -> int:
+    return tuple(sorted(sum(t) for t in input))[-1]
+
+
+with open("input.txt", 'r') as f:
+    print(solve(tuple_str_to_int(parse_input(f))))
--- 2022/utils.py
+++
@@ -1,4 +0,0 @@
-from typing import TextIO
-
-def parse_input(aoc_input: TextIO) -> tuple:
-    return (1,2)
