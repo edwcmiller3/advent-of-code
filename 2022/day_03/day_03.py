df472493d0266f078229181c0763848d4e52ffb3
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-04 11:08:12 -0500

    day 3 solved plus tests

--- 2022/day_03/day_03.py
+++ 2022/day_03/day_03.py
@@ -1,27 +1,28 @@
-# read each line of input (tuple of strings?)
-# split each line into 2 strings (part_a = line[int(midpoint):])
-# use counter? to count occurences? - see 2021 similar puzzle (Counter().most_common())
-# easier - use list comprehension
-# more fun - try set intersection
-#
-# string1 = 'abcd'
-# string2 = 'abzx'
-# common_characters = ''.join(
-#     set(string1).intersection(string2)
-# )
-# print(common_characters)  
-
-
-
 from typing import TextIO, Tuple
+import string
+
 
 def parse_input(file: TextIO) -> Tuple[str]:
     return tuple(file.read().strip().split("\n"))
 
-def split_rucksack(rucksack: Tuple[str]) -> Tuple[Tuple[str]]:
-    return True
+
+def string_middle(string: str) -> int:
+    return len(string) // 2
+
+
+def split_rucksack(rucksack: str) -> Tuple[str, str]:
+    return (rucksack[:string_middle(rucksack)], rucksack[string_middle(rucksack):])
+
+
+def shared_items(rucksacks: Tuple[str, str]) -> str:
+    return ''.join(set(rucksacks[0]).intersection(rucksacks[1]))
+
+
+def priority(item: str) -> int:
+    return string.ascii_letters.find(item) + 1
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        print(True)
+        print(
+            f"Part 1: {sum((priority(shared_items(split_rucksack(item))) for item in parse_input(f)))}")
---
+++ 2022/day_03/test_day_03.py
@@ -0,0 +1,25 @@
+from day_03 import *
+
+
+def test_priority():
+    item = 'a'
+    result = priority(item)
+    assert result == 1
+
+
+def test_shared_items():
+    rucksacks = ('abc', 'cde')
+    result = shared_items(rucksacks)
+    assert result == 'c'
+
+
+def test_split_rucksack():
+    rucksack = 'abccde'
+    result = split_rucksack(rucksack)
+    assert result == ('abc', 'cde')
+
+
+def test_string_middle():
+    string = 'abccde'
+    result = string_middle(string)
+    assert result == 3
