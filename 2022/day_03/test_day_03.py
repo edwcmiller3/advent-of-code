19988d4cae724d649cda1c6cd675f51d2f509929
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-04 23:10:16 -0500

    add test

--- 2022/day_03/test_day_03.py
+++ 2022/day_03/test_day_03.py
@@ -1,25 +1,34 @@
-from day_03_part_01 import *
+import day_03_part_01
+import day_03_part_02
 
+# Part 1 Tests
 
 def test_priority():
     item = 'a'
-    result = priority(item)
+    result = day_03_part_01.priority(item)
     assert result == 1
 
 
 def test_shared_items():
     rucksacks = ('abc', 'cde')
-    result = shared_items(rucksacks)
+    result = day_03_part_01.shared_items(rucksacks)
     assert result == 'c'
 
 
 def test_split_rucksack():
     rucksack = 'abccde'
-    result = split_rucksack(rucksack)
+    result = day_03_part_01.split_rucksack(rucksack)
     assert result == ('abc', 'cde')
 
 
 def test_string_middle():
     string = 'abccde'
-    result = string_middle(string)
+    result = day_03_part_01.string_middle(string)
     assert result == 3
+
+# Part 2 Tests
+
+def test_common_among_group():
+    group = ('abc', 'cde', 'cfg')
+    result = day_03_part_02.common_among_group(group)
+    assert result == 'c'
