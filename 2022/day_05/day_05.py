98234934dbbaa2cb4f16d0685894236c6c2a8405
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-06 22:12:31 -0500

    collin helped but still not sure how to do this

--- 2022/day_05/day_05.py
+++ 2022/day_05/day_05.py
@@ -1,4 +1,6 @@
-from typing import TextIO, Tuple
+from types import MappingProxyType
+from typing import TextIO, Tuple, Dict
+import typing
 import re
 
 # (move 2) (from 4) (to 6)
@@ -6,27 +8,39 @@ import re
 
 # import re
 # re.split(r'(?<=\d)\D', 'move 2 from 4 to 6') -> ['move 2', 'from 4', 'to 6']
+# 'move 2 from 4 to 6' -> ({'move': 2, 'from': 4, 'to': 6}, {...})
 
 # find a digit where the next character is a non digit
 # in 'move 2 from 4 to 6' it matches the space between '2' and 'from', and '4' and 'to'
 SPLIT_RULE = r'(?<=\d)\D'
 
-def read_file(file: TextIO) -> Tuple[Tuple[str, ...]]:
-    """Read and clean a file returning contents as a tuple of tuples"""
-    return tuple(tuple(ag.split(',')) for ag in file.read().strip().split("\n"))
 
+def read_file(file: TextIO) -> Tuple[str, ...]:
+    """
+    'move 2 from 3 to 4\n...' -> ('move 2 from 3 to 4', ...)
+    """
+    return tuple(file.read().strip().split('\n'))
+
+
+def generate_command(commands):
+    """
+    ('move 2 from 3 to 4', ...) -> ({'move': 2, 'from': 3, 'to': 4}, ...)
+    """
+    return tuple({k: int(v) for (k, v) in zip(command.split(' ')[::2], command.split(' ')[1::2])} for command in commands)
 
 
 if __name__ == "__main__":
-    stacks = {
-        # left to right = bottom to top
-        1: "NBDTVGZJ",
-        2: "SRMDWPF",
-        3: "VCRSZ",
-        4: "RTJZPHG",
-        5: "TCJNDZQF",
-        6: "NVPWGSFM",
-        7: "GCVBPQ",
-        8: "ZBPN",
-        9: "WPJ"
-    }
+    stacks = MappingProxyType(
+        {
+            # left to right = bottom to top
+            1: "NBDTVGZJ",
+            2: "SRMDWPF",
+            3: "VCRSZ",
+            4: "RTJZPHG",
+            5: "TCJNDZQF",
+            6: "NVPWGSFM",
+            7: "GCVBPQ",
+            8: "ZBPN",
+            9: "WPJ"
+        }
+    )
