0806651ba6bebf023c8cd6fddc650a395c20af8c
edwcmiller3 <edwcmiller3@outlook.com>
2022-12-07 22:44:22 -0500

    gave up and used loop

--- 2022/day_06/day_06.py
+++ 2022/day_06/day_06.py
@@ -1,34 +1,20 @@
-from typing import TextIO
-
-
+from collections import deque
+from itertools import islice
+from typing import TextIO, Tuple
 
 
 def read_input(file: TextIO) -> str:
     return file.read().strip()
 
 
-# def check_duplicates(chunk: str, size: int) -> bool:
-#     return True if len(set(chunk)) == size else False
-
 def unique_chunk_length(chunk: str) -> int:
     return len(set(chunk))
 
 
-
-def parse_sequence(sequence: str, size: int) -> int:
-    yield next [i + size for i in range(len(sequence)) if unique_chunk_length(sequence[i:i + size]) == size]
-    #return i + size if (unique_chunk_length(sequence[i:i + size]) == size) for i in range(len(sequence)) else False
-
-# from https://stackoverflow.com/a/73094705
-# def sliding_window(iterable, n):
-#     # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
-#     it = iter(iterable)
-#     window = collections.deque(islice(it, n), maxlen=n)
-#     if len(window) == n:
-#         yield tuple(window)
-#     for x in it:
-#         window.append(x)
-#         yield tuple(window)
+def find_signal(sequence: str, size: int):
+    for n in range(size, len(sequence)):
+        if unique_chunk_length(sequence[n - size:n]) == size:
+            return n
 
 
 # leaving for posterity since it was a nice idea
@@ -41,6 +27,4 @@ def parse_sequence(sequence: str, size: int) -> int:
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        print(f"Part 1: {parse_sequence(read_input(f), 4)}")
-# if check dupes == True return end (index of fr (+1?))
-# need to pass somewhere the inpu
+        print(f"Part 1: {find_signal(read_input(f), 4)}")
--- 2022/day_06/test_day_06.py
+++ 2022/day_06/test_day_06.py
@@ -3,12 +3,7 @@ import day_06
 
 # Test Part 1
 
-def test_check_duplicates_return_false():
-    sequence = "abcbd"
-    result = day_06.check_duplicates(sequence)
-    assert result == False
-
-def test_check_duplicates_return_true():
-    sequence = "abcd"
-    result = day_06.check_duplicates(sequence)
-    assert result == True
+def test_unique_chunk_length():
+    sequence = 'abcbd'
+    result = day_06.unique_chunk_length(sequence)
+    assert result == 4
